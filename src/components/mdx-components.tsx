cbcdb6677a8859d9e01b561cf7aa02474e831b6d
maidang1 <497350746@qq.com>
2023-08-05 18:46:23 +0800

    增加 workspace

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -3,6 +3,7 @@ import { TagLink } from "./link"
 import { BlogItem } from "./blog-item"
 import { CodeInline } from "./code-inline"
 import { Blockquote } from "./block-quote"
+import { WorkSpacePage } from "./workspace-item"
 
 export const MDXComponents = {
   ExtraTitle,
@@ -10,6 +11,7 @@ export const MDXComponents = {
   BlogItem,
   code: CodeInline,
   blockquote: Blockquote,
+  WorkSpacePage,
 } as const
 for (let key in MDXComponents) {
   if (MDXComponents.hasOwnProperty(key)) {
--- src/pages/layout/header.tsx
+++ src/pages/layout/header.tsx
@@ -10,7 +10,7 @@ const LayoutHeader = () => {
     },
     {
       text: "Workspace",
-      link: "/post",
+      link: "/workspace",
     },
   ]
   return (
